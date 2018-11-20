ba7afb5c9514a27c1ad980abda99a13de995fe87
Desiree Johnson <Desiree.Johnson@ge.com>
2018-11-20 18:01:16 -0500

    new edits

--- test.js
+++ test.js
@@ -1,19 +1,19 @@
 //Unit Tests
 describe('addValue', function() {
-    it('should return the sum of two whole numbers', function() {
-      expect(addValue(2, 4)).to.equal(6);
+    it('should return the sum of all the food content', function() {
+      expect(addValue(8, 2)).to.equal(10);
     });
   
     it('should return the sum of two decimal numbers', function() {
-      expect(addValue(2.5, 1.5)).to.equal(4);
+      expect(subtractValue(2.5, 1.5)).to.equal(4);
     });
   
     it('should return the sum of a decimal number and a string that is coercible to a number', function() {
-      expect(addValue(2.5, '1.5')).to.equal(4);
+      expect(subtractValue(2.5, '1.5')).to.equal(4);
     });
   
     it('should return the original total for invalid inputs', function() {
-      expect(addValue(4, 'cow')).to.equal(4);
+      expect(subtractValue(3, 'weight')).to.equal(4);
     });
   });
   
